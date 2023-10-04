fc04738a63c55e80275ba5be3b49e0055d4062fd
Khemchandra Persadsingh <khemchandra@gmail.com>
2023-10-04 11:47:02 -0400

    service for metadata - retrieve metadata schemas and fields from dspace server and methods to serve these to the UI

---
+++ metadataservice.py
@@ -0,0 +1,41 @@
+from dspacerequest import PublicDspaceRequest
+from config import ImporterConfig
+from metadata import MetadataSchema, MetadataField
+
+class MetadataService:
+    metadata_schemas = {}
+    _self = None
+
+    def __new__(cls, *args, **kwargs):
+        """ creates a singleton object, if it is not created,
+        or else returns the previous singleton object"""
+        if cls._self is None:
+            cls._self = super().__new__(cls)
+        return cls._self
+    
+    def __init__(self, config: ImporterConfig):
+        self.__config = config
+        self.__public_dspace_request = PublicDspaceRequest(config.dspace_rest_url())
+    
+    def populate_metadata_schemas(self) -> None:
+        # send request to dspace
+        # throws error to be caught
+        schemas = self.__public_dspace_request.get_metadata_schemas()
+        # loop over schemas json, create a metadata schema, get the fields and add to metadata schema
+        for schema in schemas["_embedded"]["metadataschemas"]:
+            # print(schema["prefix"])
+            self.metadata_schemas[schema["prefix"]] = MetadataSchema(schema["prefix"])
+            schema_fields = self.__public_dspace_request.get_metadata_schema_fields(schema["prefix"])
+            if schema_fields["page"]["totalElements"] > 0:
+                for field in schema_fields["_embedded"]["metadatafields"]:
+                    # print(f"add field - {field['id']}, {field['element']}, {field['qualifier']}")
+                    self.metadata_schemas[schema["prefix"]].add(MetadataField(field["id"], field["element"], field["qualifier"]))
+
+    def get_schemas(self) -> list:
+        return list(self.metadata_schemas)
+    
+    def get_schema_fields(self, prefix) -> list:
+        fields = []
+        for name in self.metadata_schemas[prefix].exact_name():
+            fields.append(name)
+        return fields
