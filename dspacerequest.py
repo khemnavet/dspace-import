d94b0ea342361a4c49819baab335d8742acb81ce
Khemchandra Persadsingh <khemchandra@gmail.com>
2023-10-04 11:27:28 -0400

    class to make public requests to dspace server and methods to get the metadata schemas and fields for a metadata schema

---
+++ dspacerequest.py
@@ -0,0 +1,34 @@
+import requests
+import json
+
+class PublicDspaceRequest:
+    _self = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls._self is None:
+            cls._self = super().__new__(cls)
+        return cls._self
+    
+    def __init__(self, dspaceRestURL):
+        self.__dspaceRestURL = dspaceRestURL
+
+    def get_metadata_schemas(self):
+        try:
+            # print("get metadata schemas")
+            req = requests.get(self.__dspaceRestURL + "/api/core/metadataschemas", headers={"Accept":"application/json"})
+            if req.status_code == requests.codes.ok:
+                #print(req.text)
+                return req.json()
+            req.raise_for_status()
+        except:
+            raise
+    
+    def get_metadata_schema_fields(self, prefix):
+        try:
+            # print(f"get metadata fields for {prefix}")
+            req = requests.get(self.__dspaceRestURL + "/api/core/metadatafields/search/bySchema", params={"schema": prefix}, headers={"Accept": "application/json"})
+            if req.status_code == requests.codes.ok:
+                return req.json()
+            req.raise_for_status()
+        except:
+            raise
