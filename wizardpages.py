abf492227d5b7b9f8c2742c17d1bd8b4d3a5922d
Khemchandra Persadsingh <khemchandra@gmail.com>
2023-10-06 16:35:58 -0400

    validation rules

--- wizardpages.py
+++ wizardpages.py
@@ -1,10 +1,12 @@
 # classes for the pages in the wizard
 
-from abc import ABC
 from gettext import GNUTranslations
-from PySide6.QtWidgets import QWizardPage, QLabel, QLineEdit, QGridLayout
+from PySide6.QtWidgets import QWizardPage, QLabel, QLineEdit, QGridLayout, QMessageBox
+from PySide6.QtGui import QRegularExpressionValidator
+from PySide6.QtCore import QRegularExpression
 
 from config import ImporterConfig
+from metadataservice import MetadataService
 
 
 class DSpaceWizardPages(QWizardPage):
@@ -25,17 +27,51 @@ class LoginPage(DSpaceWizardPages):
         self.setSubTitle(_("login_page_subtitle"))
 
         username_label = QLabel(text=_("login_page_username_label"))
-        username_edit = QLineEdit()
+        self.username_edit = QLineEdit()
+
+        # validator for username - email address
+        re = QRegularExpression("\\b[A-Z0-9._%+-]+@[A-Z0-9.-]+\\.[A-Z]{2,4}\\b", QRegularExpression.CaseInsensitiveOption)
+        validator = QRegularExpressionValidator(re)
+        self.username_edit.setValidator(validator)
+        #username_edit.textChanged.connect(self.__adjust_text_color(username_edit))
 
         password_label = QLabel(text=_("login_page_password_label"))
-        password_edit = QLineEdit()
-        password_edit.setEchoMode(QLineEdit.Password)
+        self.password_edit = QLineEdit()
+        self.password_edit.setEchoMode(QLineEdit.Password)
 
         layout = QGridLayout()
         layout.addWidget(username_label, 0, 0)
-        layout.addWidget(username_edit, 0, 1)
+        layout.addWidget(self.username_edit, 0, 1)
         layout.addWidget(password_label, 1, 0)
-        layout.addWidget(password_edit, 1, 1)
+        layout.addWidget(self.password_edit, 1, 1)
 
         self.setLayout(layout)
+
+        # register the fields and make them required
+        self.registerField("username*", self.username_edit)
+        self.registerField("password*", self.password_edit)
+
+
+    def __adjust_text_color(self, editor):
+        if not editor.hasAcceptableInput():
+            editor.setStyleSheet("QLineEdit { color: red; }")
+        else:
+            print("has acceptable input")
+            editor.setStyleSheet("QLineEdit { color: black; }")
+    
+    def validatePage(self) -> bool:
+        # this is called when next or finished is clicked
+        # since registerField is used and the fields are required can only check that they are valid to login to dspace
+        # validator on username does not validate string if field not in registerField?
+        # return the tokens
+        # setup a data structure where the tokens from the login are saved and can be refreshed
+        if not(len(self.username_edit.text()) > 0 and len(self.password_edit.text()) > 0):
+            msgBox = QMessageBox()
+            msgBox.setText("The username and password are required")
+            msgBox.setIcon(QMessageBox.Critical)
+            msgBox.exec()
+            return False
+        return len(self.username_edit.text()) > 0 and len(self.password_edit.text()) > 0
+        
+
     
