1ad1710ebbb30785a36530de7f055400092320fc
Khemchandra Persadsingh <khemchandra@gmail.com>
2023-10-15 17:14:09 -0400

    method to get the sub communities of a community. Also save the top communities

--- communityservice.py
+++ communityservice.py
@@ -30,7 +30,41 @@ class CommunityService:
             if comms["page"]["totalElements"] > 0:
                 for community in comms["_embedded"]["communities"]:
                     dsObject = DSO(community["uuid"], community["name"], None, DSOTypes.COMMUNITY)
-                    self.__shared_data.add_community_and_collections(community["uuid"], dsObject)
+                    self.__shared_data.add_community_and_collections(dsObject)
+                    self.__shared_data.add_top_community(dsObject.id)
         except HTTPError as err:
             print(f"Exception when getting top communities. Error code {err.response.status_code}, reason {err.response.reason}")
             raise CommunityException(err.response.reason)
+        
+    def get_subcommunities(self, community: DSO):
+        result = []
+        if community is None:
+            # return top communities
+            print("return top communities")
+            for c_id in self.__shared_data.top_communities:
+                result.append(self.__shared_data.communities_and_collections[c_id])
+        else:
+            # may have sub communities cached
+            print("return cached sub communities")
+            comm_dso = self.__shared_data.communities_and_collections[community.id]
+            if comm_dso.itemsLoaded and len(comm_dso.children) > 0:
+                # have sub communities to return
+                for c_id in comm_dso.children:
+                    result.append(self.__shared_data.communities_and_collections[c_id])
+            elif not comm_dso.itemsLoaded:
+                # request these from server
+                print("get sub communities from server")
+                try:
+                    comms = self.__community_request.sub_communities(comm_dso.uuid)
+                    if comms["page"]["totalElements"] > 0:
+                        for sub_comm in comms["_embedded"]["subcommunities"]:
+                            dsObject = DSO(sub_comm["uuid"], sub_comm["name"], comm_dso.id, DSOTypes.COMMUNITY)
+                            self.__shared_data.add_community_and_collections(dsObject)
+                            comm_dso.addChild(dsObject.id)
+                            result.append(dsObject)
+                    comm_dso.itemsLoaded = True
+                except HTTPError as err:
+                    print(f"Exception getting sub communities for {community.name}. Error code {err.response.status_code}, reason {err.response.reason}")
+                    raise CommunityException(err.response.reason)
+                
+            return result
