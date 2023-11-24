d4e9e55f2c70a801d15ef6e6a1cc909aa2f637c5
Khemchandra Persadsingh <khemchandra@gmail.com>
2023-11-24 16:29:38 -0400

    get item files

--- fileservice.py
+++ fileservice.py
@@ -9,13 +9,15 @@ class ItemFileService:
             cls._self = super().__new__(cls)
         return cls._self
     
-    def item_file_exists(self, file_name: str, file_name_matching: ItemFileMatchType, file_extension: str, item_directory: str):
+    def item_file_exists(self, file_name: str, file_name_matching: ItemFileMatchType, file_extension: str, item_directory: str) -> bool:
+        return len(self.item_files(file_name, file_name_matching, file_extension, item_directory)) > 0
+    
+    def item_files(self, file_name: str, file_name_matching: ItemFileMatchType, file_extension: str, item_directory: str) -> list:
         item_dir = Path(item_directory)
-        
+
         if file_name_matching == ItemFileMatchType.EXACT:
             file = item_dir/(file_name.strip()+file_extension)
-            return file.exists()
+            return [file] if file.exists() else []
         
         if file_name_matching == ItemFileMatchType.BEGINS:
-            file = list(item_dir.glob(file_name+"*"+file_extension))
-            return len(file) > 0
+            return list(item_dir.glob(file_name+"*"+file_extension))
