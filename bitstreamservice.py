015c7e12179c812ed9ba8367411d94b69061398d
Khemchandra Persadsingh <khemchandra@gmail.com>
2023-11-27 17:05:53 -0400

    add parameters to __new__ for parameters passed to __init__

--- bitstreamservice.py
+++ bitstreamservice.py
@@ -14,7 +14,7 @@ class BitstreamException(Exception):
 class BitstreamService:
     _self = None
 
-    def __new__(cls):
+    def __new__(cls, *args, **kwargs):
         if cls._self is None:
             cls._self = super().__new__(cls)
         return cls._self
