41dd9fe22368011fdeaab09a96e7466d8a40c695
Khemchandra Persadsingh <khemchandra@gmail.com>
2024-07-04 17:33:04 -0400

    remove backslash from re - gives invalid escape sequence when using python 3.12

--- utils.py
+++ utils.py
@@ -6,7 +6,7 @@ class Utils:
 
     @staticmethod
     def valid_uuid(uuid):
-        _match = re.match('^[a-z0-9]{8}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{4}\-[a-z0-9]{12}$', uuid)
+        _match = re.match('^[a-z0-9]{8}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{4}-[a-z0-9]{12}$', uuid)
         if _match is None:
             return False
         return True
